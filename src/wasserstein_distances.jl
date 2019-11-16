b4c28367f6590e58821ae1fb0294faaad532fe34
Yossi Bokor <yossi.bokor@anu.edu.au>
2019-11-16 14:51:16 +1100

    Improved the compatability with Barcodes from Eirene

--- src/wasserstein_distances.jl
+++ src/wasserstein_distances.jl
@@ -136,78 +136,77 @@ end
 
 function wasserstein_distance(dgm1,dgm2; p = 2,q=p)
 
-	u1 = transpose(dgm1)
-	u2 = transpose(dgm2)
-
-    #=
-    takes two (possibly unequal size) vectors and calculates the W_(q,p)distance between their persistence diagrams. The default is that q=p=2
-    Can calculate lp distance between diagrams, l1 should be the fastest.
+	u1 = dgm1
+	u2 = dgm2
+	
+	#=
+	takes two (possibly unequal size) vectors and calculates the W_(q,p)distance between their persistence diagrams. The default is that q=p=2
+	Can calculate lp distance between diagrams, l1 should be the fastest.
 	Can handle values of Inf in vectors.
-    =#
-
-    #if no Inf is present in either vector calculate as normal.
-    if all(i->(i!=Inf), u1) && all(i->(i!=Inf), u2)
+	=#
+	
+	#if no Inf is present in either vector calculate as normal.
+	if all(i->(i!=Inf), u1) && all(i->(i!=Inf), u2)
 		v1,v2,n1,n2 = pad(u1,u2)
-
+	
 		cost = dist_mat(v1,v2,n1,n2,p=p)
 		assignment = hungarian(cost)[1]
-
+	
 		if q == Inf
 			values = [cost[i, assignment[i]] for i in 1:(n1+n2)]
 			distance = maximum(values)
 			return distance
-
+	
 		else
 			distance = 0
 			for i in 1:length(assignment)
-            	distance += cost[i, assignment[i]]^(q)
-	# I forgot that whilst Eirene processeprs points as nd x np matrices, the barcodes are np x 2
+				distance += cost[i, assignment[i]]^(q)
 			end
 			return distance^(1/q)
-
+	
 		end
-
-
-
-
-    #if there are equal amounts of infinity calculate possibly finite distance.
-    elseif sum(u1[:,2] .== Inf) == sum(u2[:,2] .== Inf)
-
-            #get the number of infinities.
-            N_inf = sum(u1[:,  	2] .== Inf)
-            #sort vectors by incresum(broadcast(abs,broadcast(-, v1[:,i], v2)),dims = 1)asing amount in y component.
-            u_sort_1 = u1[:, sortperm(u1[:,2], rev = true)]
-            u_sort_2 = u2[:, sortperm(u2[:,2], rev = true)]
-            #split into infinity part and finite part
-            u_sort_1_1 = u_sort_1[1:N_inf,:]
-            u_sort_2_1 = u_sort_2[1:N_inf,:]
-            u_sort_1_2 = u_sort_1[(1+N_inf):end,:]
-            u_sort_2_2 = u_sort_2[(1+N_inf):end,:]
-
-            #calculate infinite cost.
+	
+	
+	
+	
+	#if there are equal amounts of infinity calculate possibly finite distance.
+	elseif sum(u1[:,2] .== Inf) == sum(u2[:,2] .== Inf)
+	
+			#get the number of infinities.
+			N_inf = sum(u1[:,  	2] .== Inf)
+			#sort vectors by incresum(broadcast(abs,broadcast(-, v1[:,i], v2)),dims = 1)asing amount in y component.
+			u_sort_1 = u1[:, sortperm(u1[:,2], rev = true)]
+			u_sort_2 = u2[:, sortperm(u2[:,2], rev = true)]
+			#split into infinity part and finite part
+			u_sort_1_1 = u_sort_1[1:N_inf,:]
+			u_sort_2_1 = u_sort_2[1:N_inf,:]
+			u_sort_1_2 = u_sort_1[(1+N_inf):end,:]
+			u_sort_2_2 = u_sort_2[(1+N_inf):end,:]
+	
+			#calculate infinite cost.
 			cost, assignment_inf = dist_inf(u_sort_1_1,u_sort_2_1)
-
+	
 			if q == Inf
 				costs = [cost[i, assignment_inf[i]] for i in 1:(N_inf)]
 				cost_inf = maximum(costs)
 			else
 				cost_inf = 0
-        		for i in 1:N_inf
-            		cost_inf += cost[i, assignment_inf[i]]^(q)
-        		end
+				for i in 1:N_inf
+					cost_inf += cost[i, assignment_inf[i]]^(q)
+				end
 			end
-            #calculate finite cost with self-reference.
-            cost_h = wasserstein_distance(u_sort_1_2,u_sort_2_2,p=p, q=q)
-
-
-            return cost_h + cost_inf
-
-    #unequal infinity return infinity.
-    else
-            return Inf
-
-    end
-
+			#calculate finite cost with self-reference.
+			cost_h = wasserstein_distance(u_sort_1_2,u_sort_2_2,p=p, q=q)
+	
+	
+			return cost_h + cost_inf
+	
+	#unequal infinity return infinity.
+	else
+			return Inf
+	
+	end
+	
 end
 
 
