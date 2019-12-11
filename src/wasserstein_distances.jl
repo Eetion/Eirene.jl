8bc90ae901e2c258fcef8cf75ed7773d9678c0cf
Yossi Bokor <yossi.bokor@anu.edu.au>
2019-12-11 23:33:33 +1100

    starting to fix bug with infinite cycles in wasserstein_distances.jl

--- src/wasserstein_distances.jl
+++ src/wasserstein_distances.jl
@@ -18,29 +18,67 @@ function pad(u1,u2)
     	=#
 
     	#check that columns of matrices match
+    	#println(size(u1))
     	@assert size(u1)[2] == size(u2)[2] == 2
+    	
+    	#need transpose as sometimes a 1D vector
+		n1 = size(u1)[1]
+		n2 = size(u2)[1]
+		# note total n = n1 + n2
+			v1 = vcat(u1, zeros(n2,2))
+			v2 = vcat(u2, zeros(n1,2))
+			#project to diagonal
+		for i = 1:n2
+				z = (v2[i,1]+v2[i,2])/2
+				v1[n1+i,1] = z
+				v1[n1+i,2] = z
+		end
+		################
+		for i = 1:n1
 
-	#need transpose as sometimes a 1D vector
-    n1 = size(u1)[1]
-	n2 = size(u2)[1]
-	# note total n = n1 + n2
-    	v1 = vcat(u1, zeros(n2,2))
-    	v2 = vcat(u2, zeros(n1,2))
-    	#project to diagonal
-	for i = 1:n2
-        	z = (v2[i,1]+v2[i,2])/2
-        	v1[n1+i,1] = z
-        	v1[n1+i,2] = z
-    end
-    ################
-    for i = 1:n1
+			z = (v1[i,1]+v1[i,2])/2
 
-        z = (v1[i,1]+v1[i,2])/2
+			v2[n2+i,1] = z
+			v2[n2+i,2] = z
 
-        v2[n2+i,1] = z
-        v2[n2+i,2] = z
+		end
 
-    end
+    	return v1,v2,n1,n2
+end
+
+function pad_infinite(u1,u2)
+
+    	#=
+    	Given 2 by n1 and 2 by n2 matrices, returns two 2 by (n1+n2) matrices.
+    	This is done by adding points to u1 by projecting the points in u2 to
+    	the diagonal {(x,y) : x = y }. This is also done to u2.
+    	=#
+
+    	#check that columns of matrices match
+    	#println(size(u1))
+    	@assert size(u1)[2] == size(u2)[2] ==1
+	
+	
+		#need transpose as sometimes a 1D vector
+		n1 = size(u1)[1]
+		n2 = size(u2)[1]
+		# note total n = n1 + n2
+			v1 = vcat(u1, zeros(n2,1))
+			v2 = vcat(u2, zeros(n1,1))
+			#project to diagonal
+		for i = 1:n2
+				#z = (v2[i,1]+v2[i,2])/2
+				v1[n1+i,1] = v2[i,1]
+				#v1[n1+i,2] = z
+		end
+		################
+		for i = 1:n1
+			#z = (v1[i,1]+v1[i,2])/2
+			v2[n2+i,1] = v1[i,1]
+			#v2[n2+i,2] = z
+		end
+		
+		
 
     	return v1,v2,n1,n2
 end
@@ -54,6 +92,7 @@ function dist_mat(v1,v2,n1,n2; p = 2)
 
     #take the length of columns, note this is always bigger than 2.
     n = size(v1)[1]
+    println("in dist_mat n is $n")
 
     #set up cost matrix
 	cost = zeros(n,n)
@@ -91,51 +130,56 @@ function dist_mat(v1,v2,n1,n2; p = 2)
 end
 
 function dist_inf(v1,v2)
-    #= else
-
+    #=
     takes in two vectors with all y points at infinity.
     returns the distance between their persitance diagrams.
     =#
-    @assert all(i->(i==Inf), v1[:,2]) == all(i->(i==Inf), v2[:,2])
+    println(size(v1))
+    #= c_1_inf = 0
+    c_2_inf = 0
+    for i in 1:size(v1)[1]
+    print(v1)
+    println("i is $i")
+	println(v1[i,2])
+		if v1[i,2] == Inf
+			c_1_inf += 1
+		end
+    end
+    
+    for i in 1:size(v2)[1]
+		if v2[i,2] == Inf
+			c_2_inf += 1
+		end
+	end
+	
+	@assert c_1_inf == c_2_inf
+	=#
+	
+	#if the point (Inf,Inf) exists return Inf.
+	if any(i->(i==Inf), v1[:,1]) || any(i->(i==Inf), v2[:,1])
 
-    #if the point (Inf,Inf) exists return Inf.
-    if any(i->(i==Inf), v1[:,1]) || any(i->(i==Inf), v2[:,1])
-
-        return Inf
-
+		return Inf
     else
-        n = size(v1)[1]
+		n = size(v1)[1]
+		
 		cost = zeros(n,n)
-
-        for i = 1:n 
-         	    for j in 1:n 
-                    cost[i,j] = abs(v1[i,1]-v2[j,1]) + abs(v1[i,2] - v2[j,2]) 
-                end
-        end
-
-        # elseif p == Inf 
-        #         for i = 1:n 
-        #                 for j in 1:n 
-        #                         cost[i,j] = maximum(broadcast(abs,v1[i,:]-v2[j,:]))
-        #                 end
-        # 		end
-    	# else
-        # 	for i = 1:n 
-        #         for j in 1:n 
-        #                 cost[i,j] = ((abs(v1[i,1]-v2[i,1])^p) )^(1/p)
-        #         end
-        # 	end
-		# end
+		for i = 1:n
+			for j in 1:n
+				cost[i,j] = abs(v1[i]-v2[j])
+			end
+		end
+		println("n is $n cost matrix from dist_inf is", cost)
 		return cost, hungarian(cost)[1]
 	end
 end
 ############# Main function #############
 
 function wasserstein_distance(dgm1,dgm2; p = 2,q=p)
-
-	u1 = dgm1
-	u2 = dgm2
 	
+	
+	u1 = vcat([0 0], dgm1) # this is to avoid issues with empty diagram parts
+	u2 = vcat([0 0], dgm2)
+	print(u1, u2)
 	#=
 	takes two (possibly unequal size) vectors and calculates the W_(q,p)distance between their persistence diagrams. The default is that q=p=2
 	Can calculate lp distance between diagrams, l1 should be the fastest.
@@ -144,9 +188,11 @@ function wasserstein_distance(dgm1,dgm2; p = 2,q=p)
 	
 	#if no Inf is present in either vector calculate as normal.
 	if all(i->(i!=Inf), u1) && all(i->(i!=Inf), u2)
+		println("padding", u1, u2)
 		v1,v2,n1,n2 = pad(u1,u2)
 	
 		cost = dist_mat(v1,v2,n1,n2,p=p)
+		println("cost matrix is", cost)
 		assignment = hungarian(cost)[1]
 	
 		if q == Inf
@@ -170,18 +216,29 @@ function wasserstein_distance(dgm1,dgm2; p = 2,q=p)
 	elseif sum(u1[:,2] .== Inf) == sum(u2[:,2] .== Inf)
 	
 			#get the number of infinities.
-			N_inf = sum(u1[:,  	2] .== Inf)
-			#sort vectors by incresum(broadcast(abs,broadcast(-, v1[:,i], v2)),dims = 1)asing amount in y component.
-			u_sort_1 = u1[:, sortperm(u1[:,2], rev = true)]
-			u_sort_2 = u2[:, sortperm(u2[:,2], rev = true)]
+			N_inf = sum(u1[:,2] .== Inf)
+			#sort vectors by increasing amount in y component.
+			u_sort_1 = copy(u1)
+			u_sort_2 = copy(u2)
+			order_1 = sortperm(u1[:,2], rev = true)
+			order_2 = sortperm(u2[:,2], rev = true)
+			
+			for i in 1:size(u1)[1]
+				u_sort_1[i,:] = u1[order_1[i],:]
+			end
+			for i in 1:size(u2)[1]
+				u_sort_2[i,:] = u2[order_2[i],:]
+			end
+			
 			#split into infinity part and finite part
-			u_sort_1_1 = u_sort_1[1:N_inf,:]
-			u_sort_2_1 = u_sort_2[1:N_inf,:]
-			u_sort_1_2 = u_sort_1[(1+N_inf):end,:]
-			u_sort_2_2 = u_sort_2[(1+N_inf):end,:]
+			u_sort_1_2 = u_sort_1[1:N_inf,:]
+			u_sort_2_2 = u_sort_2[1:N_inf,:]
+			u_sort_1_1 = u_sort_1[(1+N_inf):end,:]
+			u_sort_2_1 = u_sort_2[(1+N_inf):end,:]
 	
 			#calculate infinite cost.
-			cost, assignment_inf = dist_inf(u_sort_1_1,u_sort_2_1)
+			println("\n now matrices are", u_sort_1_1,u_sort_1_2,u_sort_2_1,u_sort_2_2)
+			cost, assignment_inf = dist_inf(u_sort_1_2,u_sort_2_2)
 	
 			if q == Inf
 				costs = [cost[i, assignment_inf[i]] for i in 1:(N_inf)]
@@ -192,13 +249,33 @@ function wasserstein_distance(dgm1,dgm2; p = 2,q=p)
 					cost_inf += cost[i, assignment_inf[i]]^(q)
 				end
 			end
-			#calculate finite cost with self-reference.
-			cost_h = wasserstein_distance(u_sort_1_2,u_sort_2_2,p=p, q=q)
+			#calculate finite cost without self-reference.
+			println("now matrices are", u_sort_1_2,u_sort_2_2)
+			v1,v2,n1,n2 = pad(u_sort_1_1,u_sort_2_1)
+			println(v1,v2,n1,n2)
+			cost = dist_mat(v1,v2,n1,n2,p=p)
+			println("cost matrix is", cost," i think it is here")
+			
+			assignment = hungarian(cost)[1]
+		
+			if q == Inf
+				values = [cost[i, assignment[i]] for i in 1:(n1+n2)]
+				distance = maximum(values)
+				cost_h = distance
+		
+			else
+				distance = 0
+				for i in 1:length(assignment)
+					distance += cost[i, assignment[i]]^(q)
+				end
+				cost_h =  distance
+			end
+			
 	
 			if q == Inf
 				return maximum(cost_h, cost_inf)
 			else
-				return (cost_h^q + cost_inf)^(1/q)
+				return (cost_h + cost_inf)^(1/q)
 			end
 	
 	#unequal infinity return infinity.
